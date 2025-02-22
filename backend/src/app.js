b625d828cff24d51b18439d603869c637968c257
AnKiT-GaRG2 <146389256+AnKiT-GaRG2@users.noreply.github.com>
2025-02-23 00:14:43 +0530

    cors fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,21 @@ const express = require('express');
 const aiRoutes = require('./routes/ai.routes')
 const cors = require('cors')
 const app = express()
-app.use(cors())
+
+const allowedOrigins =['http://localhost:5173'] 
+
+const corsOption =  {
+    origin: 
+      (origin, callback) => {
+          if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true); // Allow request
+          } else {
+            callback(new Error('Not allowed by CORS')); // Deny request
+          }
+        },
+    credentials: true // Allow credentials like cookies
+};
+app.use(cors(corsOption))
 app.use(express.json())
 app.get('/', (req, res) => {
     res.send('Hello World')
