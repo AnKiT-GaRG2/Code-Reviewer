2e0d534cb0ba4275d8a0f083a971067d54c831e9
AnKiT-GaRG2 <146389256+AnKiT-GaRG2@users.noreply.github.com>
2025-02-23 00:21:48 +0530

    added frontend url

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,18 +4,18 @@ const aiRoutes = require('./routes/ai.routes')
 const cors = require('cors')
 const app = express()
 
-const allowedOrigins =['http://localhost:5173'] 
+const allowedOrigins =['https://code-reviewer-frontend-mu.vercel.app'] 
 
 const corsOption =  {
     origin: 
       (origin, callback) => {
           if (allowedOrigins.includes(origin) || !origin) {
-            callback(null, true); // Allow request
+            callback(null, true); 
           } else {
-            callback(new Error('Not allowed by CORS')); // Deny request
+            callback(new Error('Not allowed by CORS')); 
           }
         },
-    credentials: true // Allow credentials like cookies
+    credentials: true 
 };
 app.use(cors(corsOption))
 app.use(express.json())
