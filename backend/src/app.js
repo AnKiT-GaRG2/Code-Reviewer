2e6842a2e3068e3d47225936bcdc65e7025780e0
AnKiT-GaRG2 <146389256+AnKiT-GaRG2@users.noreply.github.com>
2025-02-23 00:10:34 +0530

    cors fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,12 +3,7 @@ const express = require('express');
 const aiRoutes = require('./routes/ai.routes')
 const cors = require('cors')
 const app = express()
-const corsOptions = {
-    origin: 'http://localhost:5173/', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-  };
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.json())
 app.get('/', (req, res) => {
     res.send('Hello World')
